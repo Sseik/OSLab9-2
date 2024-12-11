d086807b6691dcc1bdf55344d8d836967f18abbe
Dmytro Hapchyn <48380610+Sseik@users.noreply.github.com>
2024-12-12 00:16:00 +0200

    Added Client

--- TestClient/TestClient.cpp
+++ TestClient/TestClient.cpp
@@ -5,10 +5,16 @@
 using namespace std;
 
 int main() {
-    HANDLE hPipe = CreateFileA("\\\\.\\pipe\\mynamedpipe", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-    char buffer[1000] = "C:/Code/OS|.zip";
-    WriteFile(hPipe, buffer, sizeof(buffer), NULL, NULL);
-    while (ReadFile(hPipe, buffer, 1000, NULL, NULL))
-        cout << buffer;
-    CloseHandle(hPipe);
+    char buffer[1000];
+    while (true) {
+        cout << "Request or 'exit': ";
+        cin >> buffer;
+        if (!strcmp("exit", buffer))
+            exit(0);
+        HANDLE hPipe = CreateFileA("\\\\.\\pipe\\mynamedpipe", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+        WriteFile(hPipe, buffer, sizeof(buffer), NULL, NULL);
+        while (ReadFile(hPipe, buffer, 1000, NULL, NULL))
+            cout << buffer;
+        CloseHandle(hPipe);
+    }
 }
